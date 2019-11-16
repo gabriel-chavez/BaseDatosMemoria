f0d48abbf03745d97267c4c56239dc5f4651b4b8
Gabriel Chavez PC <gabriel.chavez.r@gmail.com>
2019-11-16 09:27:13 -0400

    Inicio

---
+++ BaseDatosMemoria/Controllers/HomeController.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using BaseDatosMemoria.Models;
+using BaseDatosMemoria.DataContext;
+using Microsoft.EntityFrameworkCore;
+
+namespace BaseDatosMemoria.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        private readonly AppDbContext _context;
+
+        public HomeController(ILogger<HomeController> logger, AppDbContext context)
+        {
+            _logger = logger;
+            _context = context;
+        }
+
+        public IActionResult Index()
+        {
+            var albums = _context.Albumes.Include(a => a.Artista).ToList();
+            return View(albums);
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ BaseDatosMemoria/DataContext/AppDbContext.cs
@@ -0,0 +1,18 @@
+﻿using BaseDatosMemoria.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BaseDatosMemoria.DataContext
+{
+    public class AppDbContext: DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options):base(options)
+        {
+        }
+        public DbSet<Album> Albumes { get; set; }
+        public DbSet<Artista> Artistas { get; set; }
+    }
+}
